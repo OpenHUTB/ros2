aff2dfd703e54243174beda6fc0ea7b23942b77a
donghaiwang <open@hutb.edu.cn>
2026-09-12 10:47:10 +0800

    后台启动roscore

--- setup.bat
+++ setup.bat
@@ -4,6 +4,39 @@
 if not exist "ros2" (
     git clone https://OpenHUTB:T8w6TYB_r71gGTP3A02B@git.code.tencent.com/OpenHUTB/ros2.git   &&  cd ros2  && git lfs pull
 ) else (
-    cd ros2  && git pull
+    cd ros2
+    git reset --hard HEAD
+    git clean -fdx
+    git pull
     git lfs pull
 )
+
+
+rem Usage: vmware [OPTION ...] [--] [configuration file(s)]
+rem where OPTIONS are:
+rem -v             Show program versionPower on when a virtual machine is
+rem -x             Same as -x but also go into full screen mode
+rem -p             Start the virtual machine paused
+rem -q             Close virtual machine at rem power off
+rem -s NAME=VALUE
+rem -f
+rem Set variable NAME to VALUEOpen a new windowStart in rem full screen mode
+rem Console connection:-H hostname-U username-P password
+rem HostUser name
+rem Password for remote connections
+
+
+rem modify .vmx file to skip confirmation dialog of "Move/Copy"
+rem uuid.action = "create"
+start "" VMware\vmware.exe -x ros_noetic_humble_gazebov11_linux_win_v1\ros-noetic-humble-gazebo11-linux-win-v1.vmx
+ping -n 31 127.0.0.1 >nul
+
+rem VMware\vmrun.exe -T ws start "ros_noetic_humble_gazebov11_linux_win_v1\ros-noetic-humble-gazebo11-linux-win-v1.vmx" nogui
+
+rem sshpass pass password to ssh command to avoid password prompt
+rem https://github.com/xhcoding/sshpass-win32/releases/download/v1.0.7/sshpass.exe
+sshpass -p password ssh -o StrictHostKeyChecking=no user@192.168.40.133 "export SVGA_VGPU10=0 && export ROS_IP=$(hostname -I | tr -d [:blank:])  && export ROS_MASTER_URI=http://$ROS_IP:11311 && /bin/bash -c 'source /opt/ros/noetic/setup.bash; roscore ' &"
+
+rem exit
+
+cd ..
